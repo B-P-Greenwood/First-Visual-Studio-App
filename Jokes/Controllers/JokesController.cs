96e1898c36deb69a3aa182f27264be53f152ad1a
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-11-10 15:29:53 +0000

    Re-added search options

--- Jokes/Controllers/JokesController.cs
+++ Jokes/Controllers/JokesController.cs
@@ -32,9 +32,16 @@ namespace Jokes.Controllers
         }
 
         // POST: Jokes/ShowSearchResults
-        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase, string SearchOption)
         {
-            return View("Index", await _context.Joke.Where(j =>j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
+            if (SearchOption == "Author")
+            {
+                return View("Index", await _context.Joke.Where(j => j.JokeAuthor.Contains(SearchPhrase)).ToListAsync());
+            }
+            return View("Index", await _context.Joke.Where(j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
+
+
+     
         }
 
         // GET: Jokes/Details/5
